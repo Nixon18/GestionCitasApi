af296b5b79896eeda58624fb4b3b4419200973fb
nixon <nixonmartinezvalencia@gmail.com>
2020-06-17 12:28:34 -0500

    se agrega proyecto web de crud

---
+++ src/main/java/com/quileia/GestionCitasApiApplication.java
@@ -0,0 +1,13 @@
+package com.quileia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionCitasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionCitasApiApplication.class, args);
+	}
+
+}
